e4e1fd8f8bd57e83a54105d115ee53cdc8dfc5b3
Ioana <59113529+ioanas14@users.noreply.github.com>
2021-05-11 21:21:56 +0300

    User registration working

---
+++ app/src/main/java/com/example/travelapp/Register.java
@@ -0,0 +1,113 @@
+package com.example.travelapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class Register extends AppCompatActivity implements View.OnClickListener {
+
+    private FirebaseAuth mAuth;
+    private Button registerUser;
+    private EditText editFullName, editPassword, editEmail;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        mAuth = FirebaseAuth.getInstance();
+
+        registerUser = findViewById(R.id.registerBtn);
+        registerUser.setOnClickListener(this);
+
+        editFullName = (EditText) findViewById(R.id.fullName);
+        editPassword = (EditText) findViewById(R.id.Password);
+        editEmail = (EditText) findViewById(R.id.email);
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.registerBtn:
+                registerUser();
+                break;
+        }
+    }
+
+    private void registerUser() {
+        String email = editEmail.getText().toString().trim();
+        String password = editPassword.getText().toString().trim();
+        String name = editFullName.getText().toString().trim();
+
+        if(name.isEmpty()){
+            editFullName.setError("Full name is required!");
+            editFullName.requestFocus();
+            return;
+        }
+
+        if(email.isEmpty()){
+            editEmail.setError("Email is required!");
+            editEmail.requestFocus();
+            return;
+        }
+
+        if(password.isEmpty()){
+            editPassword.setError("Password is required!");
+            editPassword.requestFocus();
+            return;
+        }
+
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            editEmail.setError("Please provide valid e-mail!");
+            editEmail.requestFocus();
+            return;
+        }
+
+        if(password.length() < 6){
+            editPassword.setError("Password must be at least 6 characters long!");
+            editPassword.requestFocus();
+            return;
+        }
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            User user = new User(name, email);
+
+                            FirebaseDatabase.getInstance().getReference("Users")
+                                    .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                                    .setValue(user).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull  Task<Void> task) {
+                                    if(task.isSuccessful()){
+                                        Toast.makeText(Register.this, "User has been registered!", Toast.LENGTH_LONG).show();
+
+                                        //redirect the user to login page
+
+                                    } else {
+                                        Toast.makeText(Register.this, "Failed to register!", Toast.LENGTH_LONG).show();
+                                    }
+                                }
+                            });
+                        }else {
+                            Toast.makeText(Register.this, "Failed to register!", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+    }
+}
---
+++ app/src/main/java/com/example/travelapp/User.java
@@ -0,0 +1,15 @@
+package com.example.travelapp;
+
+public class User {
+    public String fullName, email;
+
+    //constructor
+    public User(){
+
+    }
+
+    public User(String fullName, String email){
+        this.fullName = fullName;
+        this.email = email;
+    }
+}

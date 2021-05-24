e536a47dfe30b2b54323258ade61d225c2fa2a7b
Ioana <59113529+ioanas14@users.noreply.github.com>
2021-05-24 13:26:18 +0300

    new trip page added, TO DO: implementation

--- app/src/main/java/com/example/travelapp/Trips.java
+++ app/src/main/java/com/example/travelapp/Trips.java
@@ -5,7 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
@@ -40,7 +43,22 @@ public class Trips extends AppCompatActivity {
             }
 
         });
+    }
 
-        //TODO implementation
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.newappmenu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch(item.getItemId()){
+            case R.id.newtrip:
+                startActivity(new Intent(Trips.this, NewTrip.class));
+                break;
+        }
+        return false;
     }
 }
